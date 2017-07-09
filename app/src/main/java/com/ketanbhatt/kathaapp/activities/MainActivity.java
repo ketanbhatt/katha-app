044ee465c6a93805750ac65b38eb07643b1f4886
simarpreetsingharora <simarpreetsingharora@gmail.com>
2017-07-09 18:11:14 +0530

    Add permission check

--- app/src/main/java/com/ketanbhatt/kathaapp/activities/MainActivity.java
+++ app/src/main/java/com/ketanbhatt/kathaapp/activities/MainActivity.java
@@ -1,8 +1,17 @@
 package com.ketanbhatt.kathaapp.activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +23,7 @@ import android.view.View;
 import com.ketanbhatt.kathaapp.R;
 import com.ketanbhatt.kathaapp.fragments.BookListFragment;
 import com.ketanbhatt.kathaapp.fragments.DonateFragment;
+import com.ketanbhatt.kathaapp.utils.Constants;
 
 /**
  * An activity representing a list of Books.
@@ -35,12 +45,45 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
+        if (
+                ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED ||
+                        ContextCompat.checkSelfPermission(
+                                this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                    (this, Manifest.permission.READ_EXTERNAL_STORAGE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Dont understand right now
+            }
+
+            ActivityCompat.requestPermissions(
+                    this, new String[]{
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },
+                    Constants.READ_WRITE_EXTERNAL_STORAGE_PERMISSION
+            );
+
+        } else {
+            initView();
+        }
+
+    }
+
+    private void initView() {
+
+
         //Navigation Drawer
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         setUpNavigationView();
-        getSupportFragmentManager().beginTransaction().add(R.id.fl_root, BookListFragment.newInstance(false)).commit();
 
+        //Books Fragment
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_root, BookListFragment.newInstance(false)).commit();
     }
 
     private void setUpNavigationView() {
@@ -95,7 +138,34 @@ public class MainActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
-
-
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Constants.READ_WRITE_EXTERNAL_STORAGE_PERMISSION: {
+                if ((grantResults.length > 0) &&
+                        (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    initView();
+                } else {
+                    Snackbar.make(findViewById(android.R.id.content), "Enable Permissions from settings",
+                            Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                            new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent = new Intent();
+                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    intent.addCategory(Intent.CATEGORY_DEFAULT);
+                                    intent.setData(Uri.parse("package:" + getPackageName()));
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                                    startActivity(intent);
+                                }
+                            }).show();
+                }
+            }
+        }
+    }
 }
+
